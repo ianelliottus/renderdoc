78499f0a8af477a10207d4c57a5a2ef297583d35
Ian Elliott <ianelliott@google.com>
2016-09-24 10:18:46 -0600

    Remove APP_C[PP]FLAGS from the "Application.mk" file.
    This seems to be overriding LOCAL_CFLAGS in the "Android.mk" file.

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -2,6 +2,4 @@ APP_ABI := armeabi-v7a arm64-v8a x86 x86_64 mips mips64
 APP_PLATFORM := android-23
 APP_STL := gnustl_static
 APP_MODULES := renderdoc renderdoccmd
-APP_CPPFLAGS += -std=c++11 -fstrict-aliasing -fvisibility=hidden -fvisibility-inlines-hidden -Wall -Werror -Wextra -Wno-unused-parameter -DVK_NO_PROTOTYES
-APP_CFLAGS += -Wall -Werror -Wextra -Wno-unused-parameter -DVK_NO_PROTOTYES
 NDK_TOOLCHAIN_VERSION := clang
